05451462e8969e7b8bcad9e2ec1036a5eb17163a
ihsanannashir <ihsanannashir@gmail.com>
2023-04-26 21:05:39 +0700

    add project sections, the data are still dummies, bottom section animation not yet completed, need to add more tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
+import ProjectSection from '../components/ProjectSection'
 
 export const metadata = {
   title: 'Ihsan An-Nashir - Portfolio',
@@ -15,6 +16,7 @@ export default function Home() {
     <main className='mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl'>
       <HeroSection/>
       <AboutSection/>
+      <ProjectSection/>
     </main>
   )
 }
