6a21696cbca69f1b5f083ed7901c0e0e0a76c989
ihsanannashir <ihsanannashir@gmail.com>
2024-09-09 13:17:12 +0700

    add container component

---
+++ src/components/cards/container/index.tsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+
+interface ContainerCardProps {
+  children: React.ReactNode;
+  className?: string;
+  variant?: "default" | "transparent";
+  title?: string;
+}
+
+const ContainerCard = ({
+  children,
+  className,
+  variant = "default",
+  title,
+}: ContainerCardProps) => {
+  const variantClassName = clsx({
+    ["bg-white rounded-lg border p-6"]: variant === "default",
+    ["bg-transparent"]: variant === "transparent",
+  });
+
+  return (
+    <section>
+      {title && (
+        <h2 className="font-bold text-xl sm:text-2xl mb-2 sm:mb-4">{title}</h2>
+      )}
+      <div className={clsx(variantClassName, className)}>{children}</div>
+    </section>
+  );
+};
+
+export default ContainerCard;
