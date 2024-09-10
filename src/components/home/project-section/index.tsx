1aed69fc4bc019ef6294e6363b25bb515a27662a
ihsanannashir <ihsanannashir@gmail.com>
2024-09-10 13:25:43 +0700

    add project sections

--- src/components/home/project-section/index.tsx
+++ src/components/home/project-section/index.tsx
@@ -1,9 +1,53 @@
-import ContainerCard from "../../cards/container";
+import { ProjectData } from "../../../lib/types/card";
+import ContainerCard from "../../cards/container-card";
+import ProjectCard from "../../cards/project-card";
+import StackShowcase from "../../stack-showcase";
+
+const PROJECTS: ProjectData[] = [
+  {
+    title: "Testing",
+    slug: "testing",
+    description: "Lorem Ipsum lorem",
+    thumbnail: "/defood.png",
+    tech: <StackShowcase nextjs tailwind />,
+  },
+  {
+    title: "Testing 2",
+    slug: "testing-2",
+    description: "Lorem Ipsum lorem 2",
+    thumbnail: "/defood.png",
+    tech: <StackShowcase nextjs tailwind expressjs />,
+  },
+  {
+    title: "Testing 3",
+    slug: "testing-3",
+    description: "Lorem Ipsum lorem 3",
+    thumbnail: "/defood.png",
+    tech: (
+      <StackShowcase react nextjs tailwind nodejs expressjs mysql postgresql />
+    ),
+  },
+];
 
 const ProjectSection = () => {
   return (
-    <ContainerCard variant="transparent" title="My Works">
-      aaa
+    <ContainerCard
+      variant="transparent"
+      title="My Projects"
+      className="grid grid-cols-1 sm:grid-cols-2 gap-6"
+    >
+      {PROJECTS.map((project, index) => {
+        return (
+          <ProjectCard
+            key={`project-card-${index}`}
+            title={project.title}
+            slug={project.slug}
+            description={project.description}
+            thumbnail={project.thumbnail}
+            tech={project.tech}
+          />
+        );
+      })}
     </ContainerCard>
   );
 };
---
+++ src/lib/types/card.d.ts
@@ -0,0 +1,7 @@
+export type ProjectData = {
+  title: string;
+  slug: string;
+  description?: string;
+  thumbnail?: string | StaticImport;
+  tech?: any;
+};
