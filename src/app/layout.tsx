879f4f7890d64e96dac62cd34a80c0dcb2f4b9b1
ihsanannashir <ihsanannashir@gmail.com>
2024-09-16 20:30:39 +0700

    improve metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,8 @@ import { TooltipProvider } from "../components/ui/tooltip";
 import Footer from "../components/footer";
 import Scroll from "../lib/utils/scroll";
 
+import OpenGraphIhsan from "../assets/images/ihsan-main.jpeg";
+
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
@@ -18,12 +20,22 @@ export const metadata: Metadata = {
   },
   description:
     "A Portfolio website of Ihsan An-Nashir, Web Developer based in Indonesia",
+  keywords:
+    "Ihsan An-Nashir, M. Ihsan An-Nashir, Freelance Web Developer, freelance web, frontend, frontend developer",
   openGraph: {
     title: "Ihsan An-Nashir's Portfolio",
     description:
       "A Portfolio website of Ihsan An-Nashir, Web Developer based in Indonesia",
     siteName: "Ihsan An-Nashir's Portfolio",
     type: "website",
+    url: `https://ihsanannashir.dev`,
+    images: [
+      {
+        url: OpenGraphIhsan.src,
+        width: OpenGraphIhsan.width,
+        height: OpenGraphIhsan.height,
+      },
+    ],
   },
 };
 
