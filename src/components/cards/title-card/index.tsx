a1841285c75b6bb7c3261f1903861da1c8c67bba
ihsanannashir <ihsanannashir@gmail.com>
2024-10-03 11:35:13 +0700

    update title card

--- src/components/cards/title-card/index.tsx
+++ src/components/cards/title-card/index.tsx
@@ -4,12 +4,21 @@ type TitleCardProps = {
   title: string;
   description?: string;
   className?: string;
+  variant?: "default" | "small";
 };
 
-const TitleCard = ({ title, description, className }: TitleCardProps) => {
+const TitleCard = ({
+  title,
+  description,
+  className,
+  variant = "default",
+}: TitleCardProps) => {
   return (
     <div className={clsx("space-y-2", className)}>
-      <h1 className="text-3xl font-bold">{title}</h1>
+      {variant === "default" && <h1 className="text-3xl font-bold">{title}</h1>}
+      {variant === "small" && (
+        <h2 className="font-bold text-xl sm:text-2xl">{title}</h2>
+      )}
       {description && <p>{description}</p>}
     </div>
   );
